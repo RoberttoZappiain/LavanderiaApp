c791a0516c4d7715a6cfd04fbb11017802b2da3a
roberttozappiain <roberttozappiain@gmail.com>
2022-11-07 15:18:14 -0700

    links

--- src/app/servicios/servicios.page.ts
+++ src/app/servicios/servicios.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.page.html',
   styleUrls: ['./servicios.page.scss'],
 })
-export class ServiciosPage implements OnInit {
+export class ServiciosPage  {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) { }
+  navigate(){
+    this.router.navigate(['/servicio-nuevo'])
   }
-
 }
